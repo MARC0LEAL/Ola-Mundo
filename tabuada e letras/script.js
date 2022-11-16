cf49243481dfa715a8d6245a4a2ef0a1d6f7b0c5
Marco Antonio S. Leal Filho <115421605+MARC0LEAL@users.noreply.github.com>
2022-11-16 00:45:26 -0300

    armazenamento por array
    foi colocado um array para armazenamento das frutas

--- tabuada e letras/script.js
+++ tabuada e letras/script.js
@@ -22,7 +22,7 @@ function salvarNumero() {
 function apagar() {
     document.getElementById('resultadoTabuada').innerHTML = ''
 }
-
+let frutasArmazenadas = []
 function confirmeFrutas() {
     let confirme = document.getElementById('fruta').value
     let resultado1 = document.getElementById('res1')
@@ -31,26 +31,23 @@ function confirmeFrutas() {
     } else {
         resultado1.innerHTML += `${confirme} <br>`
     }
+    frutasArmazenadas.push(confirme)
 }
+let resultado3 = document.getElementById('res3')
+let resultato4 = document.getElementById('res4')
+let resultado5 = document.getElementById('res5')
+let resultado6 = document.getElementById('res6')
+let resultado7 = document.getElementById('res7')
+let resultado8 = document.getElementById('res8')
 function botao1() {
-    let frutas = ['banana', 'maçã', 'uva', 'abacate', 'morango', 'goiaba', 'melão', 'mamão', 'melancia', 'limão']
     let resultado2 = document.getElementById('res2')
-    let resultado3 = document.getElementById('res3')
-    let resultato4 = document.getElementById('res4')
-    let resultado5 = document.getElementById('res5')
-    let resultado6 = document.getElementById('res6')
-    let resultado7 = document.getElementById('res7')
-    let resultado8 = document.getElementById('res8')
-    resultado2.innerHTML = 'Lista das frutas<br>'
-    for (c in frutas) {
-        resultado2.innerHTML += `${frutas[c]}<br>`
-    }
-    resultado3.innerHTML = `frutas com 3 palavras <br>${frutas.filter(a => a.length == 3)}`
-    resultato4.innerHTML = `frutas com 4 palavras <br>${frutas.filter(a => a.length == 4)}`
-    resultado5.innerHTML = `frutas com 5 palavras <br>${frutas.filter(a => a.length == 5)}`
-    resultado6.innerHTML = `frutas com 6 palavras <br>${frutas.filter(a => a.length == 6)}`
-    resultado7.innerHTML = `frutas com 7 palavras <br>${frutas.filter(a => a.length == 7)}`
-    resultado8.innerHTML = `frutas com 8 palavras <br>${frutas.filter(a => a.length == 8)}`
+    resultado2.innerHTML = `Lista das frutas escolhidas<br>${frutasArmazenadas}`
+   resultado3.innerHTML =`frutas com 3 letras <br>${frutasArmazenadas.filter(a => a.length == 3)}`
+    resultato4.innerHTML = `frutas com 4 letras <br>${frutasArmazenadas.filter(a => a.length == 4)}`
+    resultado5.innerHTML = `frutas com 5 letras <br>${frutasArmazenadas.filter(a => a.length == 5)}`
+    resultado6.innerHTML = `frutas com 6 letras <br>${frutasArmazenadas.filter(a => a.length == 6)}`
+    resultado7.innerHTML = `frutas com 7 letras <br>${frutasArmazenadas.filter(a => a.length == 7)}`
+    resultado8.innerHTML = `frutas com 8 letras <br>${frutasArmazenadas.filter(a => a.length == 8)}`
 }
 function audio() {
     document.getElementById('audio').src = 'arquivos/audio.mp3'
